ae67ba7a8757ce6710f818d5a1c8e8aa60d7fe8e
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-02-01 15:31:24 +0100

    ✅ Fixing general test app file

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const AppView());
+      await tester.pumpWidget(const App());
       expect(find.byType(CounterPage), findsOneWidget);
     });
   });
