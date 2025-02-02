9923ab93c8fca949d972bdcdbcbda35a17ec6b4b
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-02-02 15:29:54 +0100

    🔧 Adding and fixing Google identifiers for new project dev and prod

--- packages/env/lib/src/env.dart
+++ packages/env/lib/src/env.dart
@@ -4,8 +4,8 @@ enum Env {
   supabaseUrl('SUPABASE_URL'),
   supabaseAnonKey('SUPABASE_ANON_KEY'),
   powerSyncUrl('POWERSYNC_URL'),
-  iOSClientId('GOOGLE_IOS_CLIENT_ID'),
-  webClientId('GOOGLE_WEB_CLIENT_ID'),
+  iOSClientId('IOS_CLIENT_ID'),
+  webClientId('WEB_CLIENT_ID'),
   ;
 
   const Env(this.value);
