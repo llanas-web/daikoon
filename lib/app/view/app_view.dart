33120a6d8eb9ed34310e3036efed05f06c0a8319
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-02-03 16:38:17 +0100

    💚 Fixing production launch mode

--- lib/app/view/app_view.dart
+++ lib/app/view/app_view.dart
@@ -1,5 +1,8 @@
+import 'package:daikoon/app/app.dart';
 import 'package:daikoon/l10n/l10n.dart';
+import 'package:env/env.dart';
 import 'package:flutter/material.dart';
+import 'package:shared/shared.dart';
 
 class AppView extends StatelessWidget {
   const AppView({super.key});
@@ -16,10 +19,8 @@ class AppView extends StatelessWidget {
       ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const SafeArea(
-        child: Scaffold(
-          body: Text('Hello World'),
-        ),
+      home: SafeArea(
+        child: Scaffold(body: Text(getIt<AppFlavor>().getEnv(Env.iOSClientId))),
       ),
     );
   }
--- packages/powersync_repository/lib/src/powersync_repository.dart
+++ packages/powersync_repository/lib/src/powersync_repository.dart
@@ -1,5 +1,4 @@
 import 'package:env/env.dart';
-import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:powersync/powersync.dart';
