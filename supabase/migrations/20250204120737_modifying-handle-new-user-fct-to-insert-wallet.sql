f5a774ceaaf4711aeca000951bd70b136abb6bf8
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-02-04 04:14:00 -0800

    🗃️ Adding wallet creation to new users

---
+++ supabase/migrations/20250204120737_modifying-handle-new-user-fct-to-insert-wallet.sql
@@ -0,0 +1,19 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$declare is_admin boolean;
+begin
+  insert into public.users (id, email, username)
+  values (new.id, new.email, split_part(new.email, '@', 1));
+
+  insert into public.wallets (user_id)
+  values (new.id);
+
+  return new;
+end;$function$
+;
+
+
