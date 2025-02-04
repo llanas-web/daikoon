d1e3bc0a0f2c95cdb0f33ced7d2b20f1df85b81e
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-02-04 15:10:17 +0100

    🗃️ Adding powersync publication on seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -3,3 +3,6 @@ SELECT vault.create_secret(
     'supabase_project_url', 
     'URL to be used for calling edge functions, this is set here because we want to develop edge functions with webhohks from database triggers in multiple environments'
 );
+
+drop publication if exists powersync;
+create publication powersync for table public.wallets, public.users, public.transactions, public.participants, public.notifications, public.friendships, public.choices, public.challenges, public.bets;
