95ea224188934caf3cdc886a57bf2a42e73754bd
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-02-04 15:38:11 +0100

    ✨ Creating app_ui packages for assets and others

---
+++ packages/app_ui/lib/app_ui.dart
@@ -0,0 +1,8 @@
+/// A Very Good Project created by Very Good CLI.
+library ap_ui;
+
+export 'src/colors/app_colors.dart';
+export 'src/constants/constants.dart';
+export 'src/spacing/app_spacing.dart';
+export 'src/theme/theme.dart';
+export 'src/typography/typography.dart';
