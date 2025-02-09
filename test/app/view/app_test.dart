4c01c11272c36412d33324a741a47300ed330207
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-02-09 14:05:45 +0100

    ✅ Adding global first test CI

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,5 +1,46 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:daikoon/app/app.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:user_repository/user_repository.dart';
 
+import 'app_test.mocks.dart';
+
+@GenerateMocks([User, UserRepository])
 void main() {
-  group('App', () {});
+  late UserRepository userRepository;
+  late User user;
+
+  setUp(() {
+    userRepository = MockUserRepository();
+    user = MockUser();
+
+    when(user.isAnonymous).thenReturn(false);
+    when(user.isNewUser).thenReturn(false);
+
+    when(userRepository.user)
+        .thenAnswer((_) => BehaviorSubject<User>.seeded(user));
+  });
+
+  test('check user properties', () {
+    when(user.isAnonymous).thenReturn(false);
+    when(user.isNewUser).thenReturn(false);
+
+    expect(user.isAnonymous, false);
+    expect(user.isNewUser, false);
+  });
+
+  testWidgets('renders AppView inside Providers', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      App(
+        user: user,
+        userRepository: userRepository,
+      ),
+    );
+
+    expect(find.byType(App), findsOneWidget);
+  });
 }
