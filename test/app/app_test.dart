b11ff6ba2395c81b597caf011dca558d4672ddfa
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-02-09 14:20:48 +0100

    💚 Fixing github CI build

--- test/app/app_test.dart
+++ test/app/app_test.dart
@@ -1,6 +1,4 @@
-import 'package:app_ui/app_ui.dart';
 import 'package:daikoon/app/app.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
