048215810f55da72528f529c8358724e76585c28
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-04-09 15:32:34 +0200

    🐛 Fixing challenged limit form

--- lib/challenge/details/widgets/challenge_limit_details.dart
+++ lib/challenge/details/widgets/challenge_limit_details.dart
@@ -161,239 +161,238 @@ class _ChallengeLimitDetailsFormState extends State<ChallengeLimitDetailsForm> {
       await challengeDetailsCubit.validateChoice(_choiceId.value!);
     }
 
-    return AppConstrainedScrollView(
-      child: Column(
-        children: [
-          Text(
-            context.l10n.challengeDetailsStatsCreatorTitle(
-              challenge.creator!.displayUsername,
-            ),
-            style: context.headlineSmall,
+    return Column(
+      children: [
+        Text(
+          context.l10n.challengeDetailsStatsCreatorTitle(
+            challenge.creator!.displayUsername,
+          ),
+          style: context.headlineSmall,
+          textAlign: TextAlign.center,
+        ),
+        SizedBox(
+          width: 300,
+          child: Text(
+            context.l10n.challengeDetailsFinishTitle(challenge.title!),
+            style: context.headlineLarge,
             textAlign: TextAlign.center,
           ),
-          SizedBox(
-            width: 300,
-            child: Text(
-              context.l10n.challengeDetailsFinishTitle(challenge.title!),
-              style: context.headlineLarge,
-              textAlign: TextAlign.center,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              context.l10n.challengeDetailsQuestionLabel,
+              style: const TextStyle(
+                fontWeight: AppFontWeight.extraBold,
+              ),
             ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                context.l10n.challengeDetailsQuestionLabel,
-                style: const TextStyle(
-                  fontWeight: AppFontWeight.extraBold,
+            ChallengeQuestionTextField(
+              initialValue: challenge.question!,
+              readOnly: true,
+            ),
+          ].spacerBetween(height: AppSpacing.md),
+        ),
+        const Divider(color: AppColors.primary),
+        ValueListenableBuilder<String?>(
+          valueListenable: _choiceId,
+          builder: (context, selectedChoiceId, _) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  context.l10n.challengeDetailsFinishChoiceLabel,
+                  style: const TextStyle(
+                    fontWeight: AppFontWeight.extraBold,
+                  ),
                 ),
+                if (challenge.choices.isNotEmpty)
+                  Column(
+                    children: [
+                      ...challenge.choices.map(
+                        (choice) {
+                          return DaikoonFormRadioItem(
+                            title: choice.value,
+                            isSelected: choice.id == selectedChoiceId,
+                            onTap: () => _choiceId.value = choice.id,
+                          );
+                        },
+                      ),
+                    ].spacerBetween(height: AppSpacing.md),
+                  ),
+              ],
+            );
+          },
+        ),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'daikoins',
+              style: TextStyle(
+                fontWeight: AppFontWeight.extraBold,
               ),
-              ChallengeQuestionTextField(
-                initialValue: challenge.question!,
-                readOnly: true,
+            ),
+            Icon(
+              Icons.check,
+              color: AppColors.secondary,
+            ),
+          ],
+        ),
+        const Divider(color: AppColors.primary),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              context.l10n.challengeDetailsLimitDateLabel,
+              style: const TextStyle(
+                fontWeight: AppFontWeight.extraBold,
               ),
-            ].spacerBetween(height: AppSpacing.md),
-          ),
-          const Divider(color: AppColors.primary),
-          ValueListenableBuilder<String?>(
-            valueListenable: _choiceId,
-            builder: (context, selectedChoiceId, _) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    context.l10n.challengeDetailsFinishChoiceLabel,
-                    style: const TextStyle(
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: DaikoonFormDateSelector(
+                    value: challenge.limitDate,
+                    readOnly: true,
+                  ),
+                ),
+                Expanded(
+                  child: DaikoonFormTimeSelector(
+                    value: challenge.limitDate,
+                    readOnly: true,
+                  ),
+                ),
+              ].spacerBetween(width: AppSpacing.md),
+            ),
+          ].spacerBetween(height: AppSpacing.md),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              context.l10n.challengeDetailsStartDateLabel,
+              style: const TextStyle(
+                fontWeight: AppFontWeight.extraBold,
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: DaikoonFormDateSelector(
+                    value: challenge.starting,
+                    readOnly: true,
+                  ),
+                ),
+                Expanded(
+                  child: DaikoonFormTimeSelector(
+                    value: challenge.starting,
+                    readOnly: true,
+                  ),
+                ),
+              ].spacerBetween(width: AppSpacing.md),
+            ),
+          ].spacerBetween(height: AppSpacing.md),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              context.l10n.challengeDetailsEndDateLabel,
+              style: const TextStyle(
+                fontWeight: AppFontWeight.extraBold,
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: DaikoonFormDateSelector(
+                    value: challenge.ending,
+                    readOnly: true,
+                  ),
+                ),
+                Expanded(
+                  child: DaikoonFormTimeSelector(
+                    value: challenge.ending,
+                    readOnly: true,
+                  ),
+                ),
+              ].spacerBetween(width: AppSpacing.md),
+            ),
+          ].spacerBetween(height: AppSpacing.md),
+        ),
+        const Divider(color: AppColors.primary),
+        Column(
+          children: [
+            Text(
+              context.l10n.challengeDetailsFinishWinnerLabel,
+              style: const TextStyle(
+                fontWeight: AppFontWeight.extraBold,
+              ),
+            ),
+            Container(
+              width: 300,
+              decoration: BoxDecoration(
+                color: AppColors.white,
+                borderRadius: BorderRadius.circular(AppRadius.xxlg),
+              ),
+              padding: const EdgeInsets.symmetric(
+                vertical: AppSpacing.xlg,
+                horizontal: AppSpacing.xlg,
+              ),
+              child: ValueListenableBuilder<String?>(
+                valueListenable: _choiceId,
+                builder: (context, choiceId, _) {
+                  final choiceBets = bets.where(
+                    (bet) => bet.choiceId == choiceId,
+                  );
+                  final participantUsernames = choiceBets
+                      .map((bet) => bet.userId)
+                      .map(
+                        (userId) => challenge.participants
+                            .firstWhere(
+                              (participant) => participant.id == userId,
+                            )
+                            .displayUsername,
+                      )
+                      .toList();
+                  return Text(
+                    participantUsernames.join(', '),
+                    style: context.headlineSmall?.copyWith(
                       fontWeight: AppFontWeight.extraBold,
                     ),
-                  ),
-                  if (challenge.choices.isNotEmpty)
-                    Column(
-                      children: challenge.choices
-                          .map((choice) {
-                            return DaikoonFormRadioItem(
-                              title: choice.value,
-                              isSelected: choice.id == selectedChoiceId,
-                              onTap: () => _choiceId.value = choice.id,
-                            );
-                          })
-                          .toList()
-                          .spacerBetween(height: AppSpacing.md),
-                    ),
-                ],
-              );
-            },
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'daikoins',
-                style: TextStyle(
-                  fontWeight: AppFontWeight.extraBold,
-                ),
+                    textAlign: TextAlign.center,
+                  );
+                },
               ),
-              Icon(
-                Icons.check,
+            ),
+          ].spacerBetween(height: AppSpacing.md),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: AppButton(
+                text: context.l10n.challengeDetailsFinishButtonLabel,
+                onPressed: validateChoice,
                 color: AppColors.secondary,
-              ),
-            ],
-          ),
-          const Divider(color: AppColors.primary),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                context.l10n.challengeDetailsLimitDateLabel,
-                style: const TextStyle(
-                  fontWeight: AppFontWeight.extraBold,
+                textStyle: UITextStyle.button.copyWith(
+                  color: context.reversedAdaptiveColor,
                 ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: DaikoonFormDateSelector(
-                      value: challenge.limitDate,
-                      readOnly: true,
-                    ),
-                  ),
-                  Expanded(
-                    child: DaikoonFormTimeSelector(
-                      value: challenge.limitDate,
-                      readOnly: true,
-                    ),
-                  ),
-                ].spacerBetween(width: AppSpacing.md),
-              ),
-            ].spacerBetween(height: AppSpacing.md),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                context.l10n.challengeDetailsStartDateLabel,
-                style: const TextStyle(
-                  fontWeight: AppFontWeight.extraBold,
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: DaikoonFormDateSelector(
-                      value: challenge.starting,
-                      readOnly: true,
-                    ),
-                  ),
-                  Expanded(
-                    child: DaikoonFormTimeSelector(
-                      value: challenge.starting,
-                      readOnly: true,
-                    ),
-                  ),
-                ].spacerBetween(width: AppSpacing.md),
-              ),
-            ].spacerBetween(height: AppSpacing.md),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                context.l10n.challengeDetailsEndDateLabel,
-                style: const TextStyle(
-                  fontWeight: AppFontWeight.extraBold,
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: DaikoonFormDateSelector(
-                      value: challenge.ending,
-                      readOnly: true,
-                    ),
-                  ),
-                  Expanded(
-                    child: DaikoonFormTimeSelector(
-                      value: challenge.ending,
-                      readOnly: true,
-                    ),
-                  ),
-                ].spacerBetween(width: AppSpacing.md),
-              ),
-            ].spacerBetween(height: AppSpacing.md),
-          ),
-          const Divider(color: AppColors.primary),
-          Column(
-            children: [
-              Text(
-                context.l10n.challengeDetailsFinishWinnerLabel,
-                style: const TextStyle(
-                  fontWeight: AppFontWeight.extraBold,
-                ),
-              ),
-              Container(
-                width: 300,
-                decoration: BoxDecoration(
-                  color: AppColors.white,
-                  borderRadius: BorderRadius.circular(AppRadius.xxlg),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  vertical: AppSpacing.xlg,
-                  horizontal: AppSpacing.xlg,
-                ),
-                child: ValueListenableBuilder<String?>(
-                  valueListenable: _choiceId,
-                  builder: (context, choiceId, _) {
-                    final choiceBets = bets.where(
-                      (bet) => bet.choiceId == choiceId,
-                    );
-                    final participantUsernames = choiceBets
-                        .map((bet) => bet.userId)
-                        .map(
-                          (userId) => challenge.participants
-                              .firstWhere(
-                                (participant) => participant.id == userId,
-                              )
-                              .displayUsername,
-                        )
-                        .toList();
-                    return Text(
-                      participantUsernames.join(', '),
-                      style: context.headlineSmall?.copyWith(
-                        fontWeight: AppFontWeight.extraBold,
-                      ),
-                      textAlign: TextAlign.center,
-                    );
-                  },
-                ),
-              ),
-            ].spacerBetween(height: AppSpacing.md),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: AppButton(
-                  text: context.l10n.challengeDetailsFinishButtonLabel,
-                  onPressed: validateChoice,
-                  color: AppColors.secondary,
-                  textStyle: UITextStyle.button.copyWith(
-                    color: context.reversedAdaptiveColor,
-                  ),
-                  style: const ButtonStyle(
-                    backgroundColor:
-                        WidgetStatePropertyAll(AppColors.secondary),
-                    padding: WidgetStatePropertyAll(
-                      EdgeInsets.symmetric(
-                        vertical: AppSpacing.lg,
-                        horizontal: AppSpacing.xxlg,
-                      ),
+                style: const ButtonStyle(
+                  backgroundColor: WidgetStatePropertyAll(AppColors.secondary),
+                  padding: WidgetStatePropertyAll(
+                    EdgeInsets.symmetric(
+                      vertical: AppSpacing.lg,
+                      horizontal: AppSpacing.xxlg,
                     ),
                   ),
                 ),
               ),
-            ],
-          ),
-        ].spacerBetween(height: AppSpacing.xlg),
-      ),
+            ),
+          ],
+        ),
+      ].spacerBetween(height: AppSpacing.xlg),
     );
   }
 }
