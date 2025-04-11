0a1e3f3a93c35f75b6d38009b33781aa28e0e5d0
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-04-11 11:20:56 +0200

    🚸 Add empty challenge widget challenge list

--- lib/challenge/list/view/challenge_list_page.dart
+++ lib/challenge/list/view/challenge_list_page.dart
@@ -5,6 +5,7 @@ import 'package:daikoon/challenge/list/challenge_list.dart';
 import 'package:daikoon_blocks_ui/daikoon_blocks_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:shared/shared.dart';
 
 class ChallengeListPage extends StatelessWidget {
@@ -49,7 +50,22 @@ class ChallengeListView extends StatelessWidget {
             BetterStreamBuilder<List<Challenge>>(
               initialData: const <Challenge>[],
               stream: challengeListCubit.fetchChallenges(),
+              errorBuilder: (context, error) => SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: AppSpacing.lg,
+                    vertical: AppSpacing.md,
+                  ),
+                  child: Text(
+                    'Erreur lors de la récupération des défis',
+                    style: UITextStyle.subtitleBold,
+                  ),
+                ),
+              ),
               builder: (context, challenges) {
+                if (challenges.isEmpty) {
+                  return const EmptyChallenges();
+                }
                 return SliverPadding(
                   padding: const EdgeInsets.symmetric(
                     horizontal: AppSpacing.lg,
@@ -68,6 +84,63 @@ class ChallengeListView extends StatelessWidget {
               },
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class EmptyChallenges extends StatelessWidget {
+  const EmptyChallenges({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverFillRemaining(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: AppSpacing.lg,
+          vertical: AppSpacing.md,
+        ),
+        child: Column(
+          children: [
+            Text(
+              'Aucun défi trouvé',
+              style: UITextStyle.subtitleBold,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: AppSpacing.md,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: AppButton(
+                      style: const ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(
+                          AppColors.secondary,
+                        ),
+                        padding: WidgetStatePropertyAll(
+                          EdgeInsets.symmetric(
+                            vertical: AppSpacing.lg,
+                            horizontal: AppSpacing.xxlg,
+                          ),
+                        ),
+                      ),
+                      textStyle: UITextStyle.buttonText.copyWith(
+                        color: context.reversedAdaptiveColor,
+                      ),
+                      text: 'Créer un défi',
+                      onPressed: () => context.pushNamed(
+                        AppRoutes.createChallenge.name,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ].spacerBetween(
+            height: AppSpacing.lg,
+          ),
         ),
       ),
     );
