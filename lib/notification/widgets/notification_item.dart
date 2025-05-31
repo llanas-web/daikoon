04a78064291b3557836a116785fc2720b9fee8e9
Boris MAURENCE <12050570+llanas@users.noreply.github.com>
2025-05-31 11:23:54 +0200

    🐛 Fixing notification redirections

--- lib/notification/widgets/notification_item.dart
+++ lib/notification/widgets/notification_item.dart
@@ -20,7 +20,7 @@ class NotificationItem extends StatelessWidget {
       onTap: () {
         context.read<NotificationsCubit>().markAsChecked(notification.id);
         if (notification.challengeId != null) {
-          context.replaceNamed(
+          context.goNamed(
             AppRoutes.challengeDetails.name,
             pathParameters: {'challengeId': notification.challengeId!},
           );
